4aa09f0557346245491012dc4b265f44785b9896
Dmitry Kretov <kretov_1996@bk.ru>
2024-02-22 12:27:12 +0300

    env variables added, non-ASCII comments deleted

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,4 +1,3 @@
-// пакеты исполняемых приложений должны называться main
 package main
 
 import (
@@ -19,17 +18,14 @@ func run() error {
 	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
 }
 
-// функция webhook — обработчик HTTP-запроса
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		// разрешаем только POST-запросы
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// установим правильный заголовок для типа данных
 	w.Header().Set("Content-Type", "application/json")
-	// пока установим ответ-заглушку, без проверки ошибок
+
 	_, _ = w.Write([]byte(`
       {
         "response": {
