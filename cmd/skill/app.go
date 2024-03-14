0d06c5f11176efb68a8ba3bbab87418418a7f113
Dmitry Kretov <kretov_1996@bk.ru>
2024-03-14 12:51:21 +0300

    now users can register

--- cmd/skill/app.go
+++ cmd/skill/app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -108,6 +109,28 @@ func (a *app) webhook(w http.ResponseWriter, r *http.Request) {
 			text = fmt.Sprintf("Сообщение от %s, отправлено %s: %s", message.Sender, message.Time, message.Payload)
 		}
 
+		// пользователь хочет зарегистрироваться
+	case strings.HasPrefix(req.Request.Command, "Зарегистрируй"):
+		// гипотетическая функция parseRegisterCommand вычленит из запроса
+		// желаемое имя нового пользователя
+		username := parseRegisterCommand(req.Request.Command)
+
+		// регистрируем пользователя
+		err := a.store.RegisterUser(ctx, req.Session.User.UserID, username)
+		// наличие неспецифичной ошибки
+		if err != nil && !errors.Is(err, store.ErrConflict) {
+			logger.Log.Debug("cannot register user", zap.Error(err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// определяем правильное ответное сообщение пользователю
+		text = fmt.Sprintf("Вы успешно зарегистрированы под именем %s", username)
+		if errors.Is(err, store.ErrConflict) {
+			// ошибка специфична для случая конфликта имён пользователей
+			text = "Извините, такое имя уже занято. Попробуйте другое."
+		}
+
 	// если не поняли команду, просто скажем пользователю, сколько у него новых сообщений
 	default:
 		messages, err := a.store.ListMessages(ctx, req.Session.User.UserID)
@@ -157,4 +180,16 @@ func (a *app) webhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	logger.Log.Debug("sending HTTP 200 response")
+}
+
+func parseSendCommand(command string) (username string, message string) {
+	return "", ""
+}
+
+func parseReadCommand(command string) (messageIdx int) {
+	return 0
+}
+
+func parseRegisterCommand(command string) (username string) {
+	return ""
 }
